c556c6ea761d8bfe26214cf262968bcc2f446870
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-05 20:40:16 -0300

    Criando metodos

--- Caneta/src/Caneta.java
+++ Caneta/src/Caneta.java
@@ -3,13 +3,41 @@ public class Caneta {
     String cor;
     int carca;
     float ponta;
-    
+    boolean tampada = true;
+
     public Caneta(String modelo, String cor, int carca, float ponta) {
         this.modelo = modelo;
         this.cor = cor;
         this.carca = carca;
         this.ponta = ponta;
     }
+
+
+    public void rabiscar(){
+        if(tampada == false){
+            
+        }
+    }
+
+    public void tampar(){
+        if(tampada == true){
+            System.out.println("A caneta já está tampada");
+        }else{
+            tampada = true;
+        }
+
+    }
+
+    public void destampar(){
+        if(tampada == true){
+            tampada = false;
+        }else{
+            System.out.println("A caneta já está destampada!");
+        }
+
+    }
+
+   
 
    
 }
--- Caneta/src/CanetaApp.java
+++ Caneta/src/CanetaApp.java
@@ -1,5 +1,6 @@
 public class CanetaApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Caneta bic =  new Caneta("Bic", "Azul", 100, 0.7f);
+        bic.destampar();
     }
 }
