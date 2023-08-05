12bc488ae63d8ef36ad690f7f4bc71e43cbe7319
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-05 20:32:20 -0300

    Update Caneta.java

--- Caneta/src/Caneta.java
+++ Caneta/src/Caneta.java
@@ -3,4 +3,13 @@ public class Caneta {
     String cor;
     int carca;
     float ponta;
+    
+    public Caneta(String modelo, String cor, int carca, float ponta) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.carca = carca;
+        this.ponta = ponta;
+    }
+
+   
 }
