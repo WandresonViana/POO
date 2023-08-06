297ecdf3967b122aeb54b4fad4037ba61131a1af
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-06 20:15:58 -0300

    Update CanetaApp.java

--- Caneta/src/CanetaApp.java
+++ Caneta/src/CanetaApp.java
@@ -2,9 +2,11 @@ public class CanetaApp {
     public static void main(String[] args) throws Exception {
         Caneta bic = new Caneta("Bic", "Azul", 100, 0.7f);
         Caneta comp = new Caneta("Compacta", "Presta", 50, 1.1f);
+        bic.status();
         bic.destampar();
         bic.rabiscar();
 
+        comp.status();
         comp.rabiscar();
 
 
