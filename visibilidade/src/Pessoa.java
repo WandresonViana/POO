ebe5e318ef019e0b7ca4845540a5d1d79396249a
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-07 20:15:01 -0300

    Update Pessoa.java

--- visibilidade/src/Pessoa.java
+++ visibilidade/src/Pessoa.java
@@ -2,4 +2,46 @@ public class Pessoa {
     private String nomePe;
     private int idade;
     public float altura;
+    private float peso;
+
+    //construtor
+    public Pessoa(String nomePe, int idade, float altura, float peso) {
+        this.nomePe = nomePe;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getNomePe() {
+        return nomePe;
+    }
+
+    public void setNomePe(String nomePe) {
+        this.nomePe = nomePe;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
 }
