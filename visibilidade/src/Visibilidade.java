275a5f1ccb1f05e744e86a6e91a3a3c5f15cd30f
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-07 20:32:07 -0300

    Maria

--- visibilidade/src/Visibilidade.java
+++ visibilidade/src/Visibilidade.java
@@ -1,5 +1,5 @@
 public class Visibilidade {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Pessoa maria = new Pessoa("Maria", 55, 1.60f, 55.6f);
     }
 }
