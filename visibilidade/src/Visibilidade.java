16b014ef7300ef71f1daa749b7c1d2c14a004500
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-07 20:35:30 -0300

    Testando o objeto Maria

--- visibilidade/src/Visibilidade.java
+++ visibilidade/src/Visibilidade.java
@@ -1,5 +1,7 @@
 public class Visibilidade {
     public static void main(String[] args) throws Exception {
         Pessoa maria = new Pessoa("Maria", 55, 1.60f, 55.6f);
+        maria.setNomePe("Teresa");
+        maria.informacoes();
     }
 }
