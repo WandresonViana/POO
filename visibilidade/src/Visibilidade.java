57633373f7c0f8745ad3e43c10b1708524bc9c7b
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-08 20:52:41 -0300

    Criando um Gol

--- visibilidade/src/Visibilidade.java
+++ visibilidade/src/Visibilidade.java
@@ -3,6 +3,8 @@ public class Visibilidade {
         Pessoa maria = new Pessoa("Maria", 55, 1.60f, 55.6f);
         maria.setNomePe("Teresa");
         maria.informacoes();
+
+        Carro gol = new Carro("Gol", maria);
         
     }
 }
