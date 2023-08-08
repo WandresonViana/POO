8e36eaa08c30f8145d1190bdba5dbceedad22ebb
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-08 20:46:17 -0300

    Construtor do carro

--- visibilidade/src/Carro.java
+++ visibilidade/src/Carro.java
@@ -3,6 +3,15 @@ public class Carro {
     private Pessoa motorista;
     private float velocidade;
     private boolean ligado;
+
+    public Carro(String modelo, Pessoa motorista) {
+        this.modelo = modelo;
+        this.motorista = motorista;
+        this.velocidade = 0.0f;
+        this.ligado = false;
+    }
+
     
+
     
 }
