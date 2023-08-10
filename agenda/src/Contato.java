af1f2bbeaf0de4e36e9f16d9720d6182e812bc1e
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-10 20:59:11 -0300

    gete e set

--- agenda/src/Contato.java
+++ agenda/src/Contato.java
@@ -2,5 +2,36 @@ public class Contato {
     private int id;
     private String nomeContato;
     private int telefone;
-    
+    private String email;
+
+
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNomeContato() {
+        return nomeContato;
+    }
+    public void setNomeContato(String nomeContato) {
+        this.nomeContato = nomeContato;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
