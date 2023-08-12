b4d6b57aa11df5c464778f922c6531df554cffb7
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-12 19:57:15 -0300

    Update Conta.java

--- BancoDD/src/Conta.java
+++ BancoDD/src/Conta.java
@@ -5,7 +5,20 @@ public class Conta {
     private float saldo;
     private boolean status;
 
+    
+
+    public Conta(int numeroConta, String tipo, Cliente dono) {
+        this.numeroConta = numeroConta;
+        this.tipo = tipo;
+        this.dono = dono;
+        this.saldo = 0;
+        this.status = false;
+    }
+
     public void abrirConta(){
+        if(this.status == true){
+            
+        }
 
     }
 
@@ -20,4 +33,51 @@ public class Conta {
     public void depositar(){
 
     }
+
+
+
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Cliente getDono() {
+        return dono;
+    }
+
+    public void setDono(Cliente dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+
+
+    
 }
