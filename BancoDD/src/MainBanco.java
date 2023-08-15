1ff1b2561d2e28d089ec4b1462dca2612823641d
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-14 23:36:10 -0300

    Update MainBanco.java

--- BancoDD/src/MainBanco.java
+++ BancoDD/src/MainBanco.java
@@ -2,7 +2,7 @@ public class MainBanco {
     public static void main(String[] args) throws Exception {
         Cliente pedro = new Cliente(25858545, "Pedro Cardoso", "pedro@pedro.com", 'M');
         Conta cpedro = new Conta(25854, "PP", pedro);
-        cpedro.abrirConta();
+        cpedro.abrirConta("PF");
         cpedro.sacar(150);
 
         cpedro.infoConta();
