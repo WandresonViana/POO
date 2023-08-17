f78cef4f5b351b0dc2a5d6e736f142152df083d5
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-17 20:23:48 -0300

    Testes PJ

--- BancoDD/src/MainBanco.java
+++ BancoDD/src/MainBanco.java
@@ -1,7 +1,7 @@
 public class MainBanco {
     public static void main(String[] args) throws Exception {
         Cliente pedro = new Cliente(25858545, "Pedro Cardoso", "pedro@pedro.com", 'M');
-        Conta cpedro = new Conta(25854, "PF", pedro);
+        Conta cpedro = new Conta(25854, "PJ", pedro);
         cpedro.abrirConta();
         cpedro.depositar(350);
         cpedro.sacar(150);
