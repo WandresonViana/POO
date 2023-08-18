97f75b87d48caabbd5ae1fb409890b97cc576e7b
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-18 20:39:38 -0300

    Update MainAgenda.java

--- agenda/src/MainAgenda.java
+++ agenda/src/MainAgenda.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class MainAgenda {
+    
     public static void main(String[] args) throws Exception {
         Contato maria = new Contato(1, "Maria", 86959529, "maria@maria.com");
        
-       
+
     }
 }
