f2ec2cb94040c6bca1d08d5d52f8254254eba297
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-18 20:33:15 -0300

    Menu não deu certo

--- agenda/src/MainAgenda.java
+++ agenda/src/MainAgenda.java
@@ -1,7 +1,7 @@
 public class MainAgenda {
     public static void main(String[] args) throws Exception {
         Contato maria = new Contato(1, "Maria", 86959529, "maria@maria.com");
-        MenuAgenda nova = new MenuAgenda();
-        nova.menuAgenda();
+       
+       
     }
 }
