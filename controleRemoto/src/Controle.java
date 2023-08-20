62f544be5eaa1e3ec266bd8c2f4f965c5d508768
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-19 23:22:25 -0300

    Implementando interface

--- controleRemoto/src/Controle.java
+++ controleRemoto/src/Controle.java
@@ -1,10 +1,10 @@
-public class Controle {
+public class Controle implements InterControle {
    private int volume;
    private boolean ligado;
    private boolean tocando;
 
 public Controle() {
-    this.volume = 30;
+    this.volume = 50;
     this.ligado = false;
     this.tocando = false;
 }
@@ -31,6 +31,66 @@ public boolean isTocando() {
 
 public void setTocando(boolean tocando) {
     this.tocando = tocando;
+}
+
+@Override
+public void ligar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+}
+
+@Override
+public void desligar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'desligar'");
+}
+
+@Override
+public void abrirMenu() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'abrirMenu'");
+}
+
+@Override
+public void fecharMenu() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'fecharMenu'");
+}
+
+@Override
+public void maisVolume() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'maisVolume'");
+}
+
+@Override
+public void menosVolume() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'menosVolume'");
+}
+
+@Override
+public void ligarMudo() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'ligarMudo'");
+}
+
+@Override
+public void desligarMudo() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'desligarMudo'");
+}
+
+@Override
+public void play() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'play'");
+}
+
+@Override
+public void pause() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'pause'");
 } 
 
 
