b3d56f1260e928b9b8ed66c12c90e726cd83fcaf
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-20 20:59:25 -0300

    Testando o controle

--- controleRemoto/src/ControMain.java
+++ controleRemoto/src/ControMain.java
@@ -1,5 +1,6 @@
 public class ControMain {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Controle c = new Controle();
+        c.abrirMenu();
     }
 }
