8c21db5cf312c1db0918aa19134d51c36c5b950a
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-19 23:20:09 -0300

    métodos especiais

--- controleRemoto/src/Controle.java
+++ controleRemoto/src/Controle.java
@@ -7,6 +7,32 @@ public Controle() {
     this.volume = 30;
     this.ligado = false;
     this.tocando = false;
+}
+
+public int getVolume() {
+    return volume;
+}
+
+public void setVolume(int volume) {
+    this.volume = volume;
+}
+
+public boolean isLigado() {
+    return ligado;
+}
+
+public void setLigado(boolean ligado) {
+    this.ligado = ligado;
+}
+
+public boolean isTocando() {
+    return tocando;
+}
+
+public void setTocando(boolean tocando) {
+    this.tocando = tocando;
 } 
+
+
    
 }
