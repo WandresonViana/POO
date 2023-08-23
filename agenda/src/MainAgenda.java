cdc937bcfa27d0c9ea5209b07478843c08a8062d
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-22 23:50:33 -0300

    Carlos

--- agenda/src/MainAgenda.java
+++ agenda/src/MainAgenda.java
@@ -4,6 +4,7 @@ public class MainAgenda {
     
     public static void main(String[] args) throws Exception {
         Contato maria = new Contato(1, "Maria", "86959529", "maria@maria.com");
+        Contato carlor = new Contato(2, "Carlos", "86995298418", "carlos@carlos.com");
         maria.infoContato();
        
 
