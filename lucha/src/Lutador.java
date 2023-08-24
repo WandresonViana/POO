623d15695d8fa4f99873d7d5b2469bf833809016
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-23 22:47:59 -0300

    Update Lutador.java

--- lucha/src/Lutador.java
+++ lucha/src/Lutador.java
@@ -5,5 +5,30 @@ public class Lutador {
     private float altura;
     private float peso;
     private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+
+    public Lutador(String nomeLutador, String nacionalidade, int idade, float altura, float peso, String categoria,
+            int vitorias, int derrotas, int empates) {
+        this.nomeLutador = nomeLutador;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.categoria = categoria;
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
+
+
+    public void apresentar(){
+        
+    }
+
+
     
+
 }
