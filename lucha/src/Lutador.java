9a581cf59637a8167081c3e9b1cbf11b798b3a86
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-23 22:36:00 -0300

    atributos

--- lucha/src/Lutador.java
+++ lucha/src/Lutador.java
@@ -1,3 +1,9 @@
 public class Lutador {
+    private String nomeLutador;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
     
 }
