5461434cf8901a81b41806b21fa161aaa87a4c79
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-23 22:51:49 -0300

    Métodos

--- lucha/src/Lutador.java
+++ lucha/src/Lutador.java
@@ -25,10 +25,119 @@ public class Lutador {
 
 
     public void apresentar(){
-        
+
+    }
+
+    public void status(){
+
+    }
+
+    public void ganharLuta(){
+
+    }
+
+    public void perderLuta(){
+
+    }
+
+    public void empatarLuta(){
+
     }
 
 
+    public String getNomeLutador() {
+        return nomeLutador;
+    }
+
+
+    public void setNomeLutador(String nomeLutador) {
+        this.nomeLutador = nomeLutador;
+    }
+
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    public float getAltura() {
+        return altura;
+    }
+
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+
+    public float getPeso() {
+        return peso;
+    }
+
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+
+    public int getEmpates() {
+        return empates;
+    }
+
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+
+
+
+    
     
 
 }
