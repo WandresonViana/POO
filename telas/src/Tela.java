caaef9caa36f141e40077b0747ee3b8a47bd1302
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-29 23:02:50 -0300

    Update Tela.java

--- telas/src/Tela.java
+++ telas/src/Tela.java
@@ -1,8 +1,12 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Tela extends JFrame{
     JLabel login = new JLabel("Login");
+    JTextField senha = new JTextField("");
+    JLabel jSenha = new JLabel("Senha");
+    
     public static void main(String[] args) {
         Tela tp = new Tela();
     }
@@ -12,12 +16,13 @@ public class Tela extends JFrame{
     }
 
     public void montarTela(){
-        this.setBounds(EXIT_ON_CLOSE, ABORT, 400, 200);
+        this.setBounds(EXIT_ON_CLOSE, 100, 400, 200);
         this.setVisible(true);
         this.setLayout(null);
         this.setTitle("Tela login");
 
         login.setBounds(30, 40, 80, 25);
+        
 
         //Adicionando a tela
         this.add(login);
