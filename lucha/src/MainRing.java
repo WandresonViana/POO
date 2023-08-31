a567dc7bd0163a021da59c213f5cd581c487fa8a
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-30 22:49:52 -0300

    Update MainRing.java

--- lucha/src/MainRing.java
+++ lucha/src/MainRing.java
@@ -1,8 +1,11 @@
 public class MainRing {
     public static void main(String[] args) throws Exception {
-        Lutador l = new Lutador("Pretty Boy","França", 31, 1.75f, 68.9f, 11, 2, 1);
+        Lutador l[] = new Lutador[6];
+        l[0] = new Lutador("Pretty Boy","França", 31, 1.75f, 68.9f, 11, 2, 1);
 
-        l.apresentar();
-        l.status();
+        l[1] = new Lutador("Putscrip", "Braisl", 29, 1.68f, 57.8f, 14, 2, 3);
+
+        l[0].apresentar();
+        l[0].status();
     }
 }
