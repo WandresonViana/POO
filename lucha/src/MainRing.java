d0225522394ebbcafd4d706a2f6e818edce441c1
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-30 22:54:18 -0300

    Update MainRing.java

--- lucha/src/MainRing.java
+++ lucha/src/MainRing.java
@@ -5,6 +5,12 @@ public class MainRing {
 
         l[1] = new Lutador("Putscrip", "Braisl", 29, 1.68f, 57.8f, 14, 2, 3);
 
+        l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
+
+        l[3] = new Lutador("Dead", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
+
+        l[4]
+
         l[0].apresentar();
         l[0].status();
     }
