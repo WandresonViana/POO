2e0934b466912c13e909425dd8ba7d01cd91bc8e
Wandreson Viana <wandresonviana01@gmail.com>
2023-08-31 22:54:41 -0300

    Update Luta.java

--- lucha/src/Luta.java
+++ lucha/src/Luta.java
@@ -4,7 +4,12 @@ public class Luta {
     private int rounds;
     private boolean aprovado; 
 
-    public void marcarLuta(){
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if(l1.getCategoria() == l2.getCategoria() && l1 != l2){
+           this.aprovado = true;
+           this.desafiado = l1;
+           this.desafiante = l2; 
+        }
 
     }
 }
