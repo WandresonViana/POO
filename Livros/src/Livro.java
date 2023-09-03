e4d46401cb5a9c42734c6f81a0ce49ff3f354225
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-03 20:16:14 -0300

    Livro

---
+++ Livros/src/Livro.java
@@ -0,0 +1,11 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int totPaginas;
+    private int paginaAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    
+    
+}
