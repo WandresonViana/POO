d9cedfcfcc797656a673c18a996bd62aaab54fd9
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-03 20:21:26 -0300

    Update Livro.java

--- Livros/src/Livro.java
+++ Livros/src/Livro.java
@@ -6,6 +6,68 @@ public class Livro {
     private boolean aberto;
     private Pessoa leitor;
 
+    //Construtor
+    public Livro(String titulo, String autor, int totPaginas, int paginaAtual, boolean aberto, Pessoa leitor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.totPaginas = totPaginas;
+        this.paginaAtual = paginaAtual;
+        this.aberto = aberto;
+        this.leitor = leitor;
+    }
+
+
+    //Get e Set
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getPaginaAtual() {
+        return paginaAtual;
+    }
+
+    public void setPaginaAtual(int paginaAtual) {
+        this.paginaAtual = paginaAtual;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
+    
+
     
     
 }
