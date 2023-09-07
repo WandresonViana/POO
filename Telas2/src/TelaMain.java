1a17527807671542cb66a5c716eb2c940fa95b24
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-07 20:10:29 -0300

    Criando a tela

--- Telas2/src/TelaMain.java
+++ Telas2/src/TelaMain.java
@@ -1,4 +1,6 @@
-public class App {
+import javax.swing.JFrame;
+
+public class TelaMain extends JFrame{
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
