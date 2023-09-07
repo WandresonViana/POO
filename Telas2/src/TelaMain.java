3e3273609c8deb6f76bbf488a758a109279d5019
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-07 20:23:39 -0300

    Update TelaMain.java

--- Telas2/src/TelaMain.java
+++ Telas2/src/TelaMain.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 public class TelaMain extends JFrame{
 
     public static void main(String[] args){
-       
+       TelaMain nova = new TelaMain();
 
     }
 
@@ -13,7 +13,11 @@ public class TelaMain extends JFrame{
     }
 
     public void telaMontar(){
-        this.setBounds(300, 300, 300, 300);
+        this.setBounds(300, 500, 450, 300);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setTitle("Login");
+
+        this.setVisible(true);
     }
 }
