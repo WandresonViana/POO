ef80a11b1e62d4eaac10aa40daa3d0e310f496d4
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-09 19:52:45 -0300

    Update LivrosMain.java

--- Livros/src/LivrosMain.java
+++ Livros/src/LivrosMain.java
@@ -8,6 +8,7 @@ public class LivrosMain {
         p[1] = new Pessoa("Maria", 45, "F");
 
         //Livros
-        
+        l[0] =  new Livro("O alto da aparecida", "Miranda Mirado", 250, p[0]);
+        l[1] = new Livro("Carlota Carolina", "Cleia Correia", 600, p[1]);
     }
 }
