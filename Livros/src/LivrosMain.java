38962c68b55f9af54f501b68bf538863b41244e7
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-09 19:50:16 -0300

    Criando livros

--- Livros/src/LivrosMain.java
+++ Livros/src/LivrosMain.java
@@ -3,5 +3,11 @@ public class LivrosMain {
         Pessoa p[] = new Pessoa[3];
         Livro l[] = new Livro[3];
         
+        //Pessoas
+        p[0] = new Pessoa("Carlos", 25, "M");
+        p[1] = new Pessoa("Maria", 45, "F");
+
+        //Livros
+        
     }
 }
