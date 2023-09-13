036b4d25b74bfb862bd02fe9f9fd1740ff5aa4d0
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-12 22:50:58 -0300

    Vetor de contatos

--- agenda/src/MainAgenda.java
+++ agenda/src/MainAgenda.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class MainAgenda {
     
-    public static void main(String[] args) throws Exception {
-        Contato maria = new Contato(1, "Maria", "86959529", "maria@maria.com");
-        Contato carlos = new Contato(2, "Carlos", "86995298418", "carlos@carlos.com");
-        maria.infoContato();
-        carlos.infoContato();
+    public static void main(String[] args){
+        Contato agendei[] = new Contato[5];
+        agendei[0] = new Contato(1, "Maria", "86959529", "maria@maria.com");
+        agendei[1] = new Contato(2, "Carlos", "86995298418", "carlos@carlos.com");
+        
+        agendei[0].infoContato();
+        agendei[1].infoContato();
        
 
     }
