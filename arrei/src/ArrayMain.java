de480041cd15cd7273f610435d958b2b85eb2b45
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-16 20:43:53 -0300

    Array de nomes

--- arrei/src/ArrayMain.java
+++ arrei/src/ArrayMain.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class ArrayMain {
     public static void main(String[] args) throws Exception {
         ArrayList<Integer> numeros = new ArrayList();
+        ArrayList<String> nomes = new ArrayList<>();
         
         numeros.add(5);
         numeros.add(4);
@@ -12,6 +13,15 @@ public class ArrayMain {
 
         for(int con:numeros){
             System.out.println(con);
+        }
+
+        nomes.add("Maria");
+        nomes.add("Pedro");
+        nomes.add("Teresa");
+        nomes.add("Vitor");
+
+        for(String n:nomes){
+            System.out.println(n);
         }
         
     }
