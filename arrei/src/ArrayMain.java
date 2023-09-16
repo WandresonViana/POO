6c30d56b89891b10b05d47ac0719240768a6aa9b
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-15 23:27:46 -0300

    Update ArrayMain.java

--- arrei/src/ArrayMain.java
+++ arrei/src/ArrayMain.java
@@ -4,5 +4,15 @@ public class ArrayMain {
     public static void main(String[] args) throws Exception {
         ArrayList<Integer> numeros = new ArrayList();
         
+        numeros.add(5);
+        numeros.add(4);
+        numeros.add(18);
+        numeros.add(14);
+        numeros.sort(null);
+
+        for(int con:numeros){
+            System.out.println(con);
+        }
+        
     }
 }
