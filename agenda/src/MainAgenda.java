af16eb3ab82e171c8a854c2bf77b7cad886128e0
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-15 23:06:08 -0300

    Update MainAgenda.java

--- agenda/src/MainAgenda.java
+++ agenda/src/MainAgenda.java
@@ -6,6 +6,7 @@ public class MainAgenda {
         Contato agendei[] = new Contato[5];
         agendei[0] = new Contato(1, "Maria", "86959529", "maria@maria.com");
         agendei[1] = new Contato(2, "Carlos", "86995298418", "carlos@carlos.com");
+        agendei[2] = new Contato(3,"Carlos", "868585989589", "carlos@carlos.com");
         
         agendei[0].infoContato();
         agendei[1].infoContato();
