c1a56358721f87ba8392d1fd2300eddba1cec925
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-23 21:57:28 -0300

    Update Animal.java

--- herança/src/Animal.java
+++ herança/src/Animal.java
@@ -2,4 +2,52 @@ public class Animal {
     private String nome;
     private boolean vivo;
     private String som;
+    private int patas;
+
+   
+
+    public Animal(String nome, boolean vivo, String som, int patas) {
+        this.nome = nome;
+        this.vivo = vivo;
+        this.som = som;
+        this.patas = patas;
+    }
+
+    public void emitirSom(){
+        System.out.println("Emitindo som!");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+
+    public String getSom() {
+        return som;
+    }
+
+    public void setSom(String som) {
+        this.som = som;
+    }
+
+    
+     public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+    
 }
