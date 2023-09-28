0882e1afb4acd79749c649552fdd6c2ece390f5e
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-27 23:35:34 -0300

    Update HerancaMain.java

--- herança/src/HerancaMain.java
+++ herança/src/HerancaMain.java
@@ -1,6 +1,7 @@
 public class HerancaMain {
     public static void main(String[] args) throws Exception {
        Cachorro dog = new Cachorro("Fido",true, "Latido", 4, "PitBull", 25);
+       Cachorro lulu = new Cachorro("Lulu", true, "Latido", 4, "Pudu", 15);
 
        dog.emitirSom();
     }
