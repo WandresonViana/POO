18c189451094ee4c5524bda5c27a13e66e341c9c
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-27 23:25:19 -0300

    Update Cachorro.java

--- herança/src/Cachorro.java
+++ herança/src/Cachorro.java
@@ -2,8 +2,11 @@ public class Cachorro extends Animal {
     private String raca;
     private boolean peso;
     
-    public Cachorro(String nome, boolean vivo, String som, int patas) {
+    public Cachorro(String nome, boolean vivo, String som, int patas, String raca, boolean peso) {
         super(nome, vivo, som, patas);
-        //TODO Auto-generated constructor stub
+        this.raca = raca;
+        this.peso = peso;
     }
+    
+   
 }
