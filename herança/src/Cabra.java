d9668a831b791449316d9172c286dd3d6f243f81
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-29 20:51:49 -0300

    Update Cabra.java

--- herança/src/Cabra.java
+++ herança/src/Cabra.java
@@ -1,3 +1,8 @@
 public class Cabra extends Animal{
+
+    public Cabra(String nome, boolean vivo, String som, int patas) {
+        super(nome, vivo, som, patas);
+        //TODO Auto-generated constructor stub
+    }
     
 }
