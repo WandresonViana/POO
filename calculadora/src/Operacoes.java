7fe6825f169052dbf13e46521793cf7ebfcc1366
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-01 00:02:34 -0300

    Create Operacoes.java

---
+++ calculadora/src/Operacoes.java
@@ -0,0 +1,11 @@
+public class Operacoes {
+    private double valor01;
+    private double valor02;
+
+    public Operacoes(){
+        this.valor01 = 0;
+        this.valor02 = 0;
+    }
+    
+    
+}
