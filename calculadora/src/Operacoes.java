b3b852735405292f5a188af8d4ec2425fcbf37aa
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-01 00:26:41 -0300

    Update Operacoes.java

--- calculadora/src/Operacoes.java
+++ calculadora/src/Operacoes.java
@@ -2,9 +2,25 @@ public class Operacoes {
     private double valor01;
     private double valor02;
 
-    public Operacoes(){
-        this.valor01 = 0;
-        this.valor02 = 0;
+    public Operacoes(double valor1, double valor2){
+        this.valor01 = valor1;
+        this.valor02 = valor2;
+    }
+
+    public double getValor01() {
+        return valor01;
+    }
+
+    public void setValor01(double valor01) {
+        this.valor01 = valor01;
+    }
+
+    public double getValor02() {
+        return valor02;
+    }
+
+    public void setValor02(double valor02) {
+        this.valor02 = valor02;
     }
     
     
