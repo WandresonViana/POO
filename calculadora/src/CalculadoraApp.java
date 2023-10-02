5213d383f4a60fefcb43e17583a64fb3940e8a51
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-01 22:14:44 -0300

    Update

--- calculadora/src/CalculadoraApp.java
+++ calculadora/src/CalculadoraApp.java
@@ -8,12 +8,16 @@ public class CalculadoraApp {
         Numero num1 =new Numero();
         Numero num2 = new Numero();
         Numero resultado = new Numero();
+        Operacoes nn = new Operacoes(num1.getValor(), num2.getValor());
 
-        System.out.print("Digite o valor 1");
+        System.out.print("Digite o valor 1 ");
         num1.setValor(scan.nextDouble());
 
         System.out.print("Digite o valor 2: ");
         num2.setValor(scan.nextDouble());
+
+        nn.somar(num1.getValor(), num2.getValor());
+        
 
     }
 }
--- calculadora/src/Operacoes.java
+++ calculadora/src/Operacoes.java
@@ -1,16 +1,16 @@
 public class Operacoes {
     private double valor01;
     private double valor02;
-    private double ress;
+    
 
     public Operacoes(double valor1, double valor2){
         this.valor01 = valor1;
         this.valor02 = valor2;
-        this.ress = 0;
+        
     }
 
-    public void somar(double valor1, double valor2){
-       this.setRess(this.getValor01() + this.getValor02());
+    public double somar(double valor01, double valor02){
+       return this.getValor01() + this.getValor02();
 
     }
 
@@ -29,14 +29,7 @@ public class Operacoes {
     public void setValor02(double valor02) {
         this.valor02 = valor02;
     }
-
-    public double getRess() {
-        return ress;
-    }
-
-    public void setRess(double ress) {
-        this.ress = ress;
-    }
+  
     
     
 }
