d67b42b8d1d8f7c2bffb3f8322deac04187d335e
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-01 21:59:25 -0300

    Update Operacoes.java

--- calculadora/src/Operacoes.java
+++ calculadora/src/Operacoes.java
@@ -1,14 +1,16 @@
 public class Operacoes {
     private double valor01;
     private double valor02;
+    private double ress;
 
     public Operacoes(double valor1, double valor2){
         this.valor01 = valor1;
         this.valor02 = valor2;
+        this.ress = 0;
     }
 
     public void somar(double valor1, double valor2){
-        
+       this.setRess(this.getValor01() + this.getValor02());
 
     }
 
@@ -26,6 +28,14 @@ public class Operacoes {
 
     public void setValor02(double valor02) {
         this.valor02 = valor02;
+    }
+
+    public double getRess() {
+        return ress;
+    }
+
+    public void setRess(double ress) {
+        this.ress = ress;
     }
     
     
