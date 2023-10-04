36aa30464ab295a1724b83bc2683c03c8403810e
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-03 23:48:14 -0300

    Update CalculadoraApp.java

--- calculadora/src/CalculadoraApp.java
+++ calculadora/src/CalculadoraApp.java
@@ -17,15 +17,27 @@ public class CalculadoraApp {
         num2.setValor(scan.nextDouble());
 
         Operacoes nn = new Operacoes(num1.getValor(), num2.getValor());
+        System.out.println("Digite a operação desejada: ");
+        op = scan.nextLine();
 
          switch(op){
             case "+":
                 resultado.setValor(nn.somar());
+                break;
             case "-":
+                resultado.setValor(nn.subtrair());
+                break;
+            case "*":
+                resultado.setValor(nn.multiplicar());
+            case "/":
+                resultado.setValor(nn.dividir());
+                break;
+            default:
+                System.out.println("Operador inválido");
         }
 
         
-        System.out.println("A soma vale " + resultado.getValor());
+        System.out.println("O resultado vale " + resultado.getValor());
 
         resultado.setValor(nn.subtrair());
         System.out.println("A subtração vale: " + resultado.getValor());
