4682b22d6dca498f0292037b05bf597413e8e13b
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-07 22:28:37 -0300

    Update Periquito.java

--- herança/src/Periquito.java
+++ herança/src/Periquito.java
@@ -1,4 +1,11 @@
-public class Periquito {
+public class Periquito extends Animal{
     private String cor;
     private int peso;
+    public Periquito(String nome, boolean vivo, String som, int patas, String cor, int peso) {
+        super(nome, vivo, som, patas);
+        this.cor = cor;
+        this.peso = peso;
+    }
+
+    
 }
