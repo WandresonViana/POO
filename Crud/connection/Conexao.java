cfb5aae1ae95c832bcf95de79bdeeffc1c0676fc
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-20 23:12:31 -0300

    Conexão criada

--- Crud/connection/Conexao.java
+++ Crud/connection/Conexao.java
@@ -1,10 +1,32 @@
 package Crud.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
     private static final String USERNAME = "root";
 
     private static final String PASSWORD = "";
 
-    
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda";
+
+
+    public static Connection createConnectionToMySQL() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+
+        return connection;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection conct = createConnectionToMySQL();
+
+        if(conct!=null){
+            System.out.println("Conectado ao banco de dados!");
+            conct.close();
+        }
+    }
     
 }
--- Crud/src/CrudApp.java
+++ Crud/src/CrudApp.java
@@ -1,5 +1,9 @@
+import java.sql.Connection;
+
+import Crud.connection.Conexao;
+
 public class CrudApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      
     }
 }
