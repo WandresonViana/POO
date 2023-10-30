ae260ae976c85f38fccc734a0d9991226bae0f4b
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-29 22:48:44 -0300

    Update CrudProduto.java

--- Crud/controller/CrudProduto.java
+++ Crud/controller/CrudProduto.java
@@ -1,5 +1,7 @@
 package Crud.controller;
 
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 
 import Crud.model.Produto;
@@ -8,8 +10,16 @@ public class CrudProduto {
 
     public List<Produto> lista(){
         String sql = "SELECT * FROM produto";
-        
-        
+        List<Produto> produtos = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = 
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+        return produtos;
     }
     
 }
