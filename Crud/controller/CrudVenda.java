547697f68c9e11f57a93a0c36bf74774d544082b
Wandreson Viana <wandresonviana01@gmail.com>
2023-11-06 23:52:21 -0300

    Update CrudVenda.java

--- Crud/controller/CrudVenda.java
+++ Crud/controller/CrudVenda.java
@@ -1,12 +1,23 @@
 package Crud.controller;
 
 import java.sql.Connection;
+import java.util.List;
 
 import Crud.connection.Conexao;
+import Crud.model.Venda;
 
 public class CrudVenda {
     Connection con = null;
     Conexao conexao = new Conexao();
 
-    
+
+    public List<Venda> listarVendas(){
+
+        
+    }
+
+    public void cadastraVenda(){
+
+    }
+
 }
