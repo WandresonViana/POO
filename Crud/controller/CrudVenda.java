f877ec73ea9f4758cd3dd0a34973b3c5d17b7338
Wandreson Viana <wandresonviana01@gmail.com>
2023-11-06 23:58:58 -0300

    Update CrudVenda.java

--- Crud/controller/CrudVenda.java
+++ Crud/controller/CrudVenda.java
@@ -3,9 +3,11 @@ package Crud.controller;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import Crud.connection.Conexao;
+import Crud.model.Produto;
 import Crud.model.Venda;
 
 public class CrudVenda {
@@ -16,6 +18,8 @@ public class CrudVenda {
 
 
     public List<Venda> listarVendas(){
+        String sql = "SELECT * FROM produto";
+        List<Produto> produtos = new ArrayList<>();
 
         return venda;
     }
